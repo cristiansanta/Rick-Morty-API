365f07bef9cc30efcf70bec5c4ee2e0e234b5665
cristiansanta <cristiansanta5@gmail.com>
2023-09-02 09:46:48 -0500

    Rick and Morty API

---
+++ index.js
@@ -0,0 +1,24 @@
+function getCharacters(done) {
+    const results = fetch("https://rickandmortyapi.com/api/character");
+
+    results
+        .then(response => response.json())
+        .then(data => {
+            done(data)
+        })
+}
+
+getCharacters(data => {
+    data.results.forEach(personaje => {
+        const article = document.createRange().createContextualFragment(/*html*/`
+        <article>
+            <div class="image-container"></div>
+            <img src="${personaje.image}" alt="Personaje">
+            <h2>${personaje.name}</h2>
+            <span>${personaje.status}</span>
+        </article>
+        `);
+        const main = document.querySelector("main");
+        main.append(article);
+    });
+});
